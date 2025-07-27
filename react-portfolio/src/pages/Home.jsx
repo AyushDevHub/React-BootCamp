28b7aba77a59fb98e1d858f40a892896a1c01740
AyushDevHub <coder.ayushshaw@gmail.com>
2025-07-27 21:45:40 +0530

    feat: initial Tailwind CSS setup with theme customization

---
+++ react-portfolio/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return(
+        <div className="min-h-screen container">
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+// Theme Toggle
+// Background-effects
+// Navbar 
+// Main content
+// footer
